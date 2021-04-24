456f12b91a9d3c4e8bd2ea7b2be538efaddc7123
XXXNKKK <82957568+XXXNKKK@users.noreply.github.com>
2021-04-25 06:49:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,8 +19,8 @@
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Define My Package
-git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
-git clone https://github.com/fw876/helloworld.git package/lean/luci-app-hellow-plus
+#git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
+#git clone https://github.com/fw876/helloworld.git package/lean/luci-app-hellow-plus
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
 #git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
